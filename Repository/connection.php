19d4226df19c21e9d5fc1df1c663a4976e1da932
LoeisDavid <86642990+LoeisDavid@users.noreply.github.com>
2025-04-13 21:12:37 +0700

    menambahkan env sekarang dan membenarkan database melalui env, memperbarui readme

--- Repository/connection.php
+++ Repository/connection.php
@@ -1,14 +1,15 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../'); // arahkan ke root
 $dotenv->load();
+
 
 $host = $_ENV['DB_HOST'];
 $user = $_ENV['DB_USER'];
 $pass = $_ENV['DB_PASS'];
-$db   = 'projectMagang1';
+$db   = $_ENV['DB_DATABASE'];
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
