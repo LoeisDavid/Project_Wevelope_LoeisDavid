d38b13e987ab9145bc5a802fb54dc1c6eea5f0cf
LoeisDavid <86642990+LoeisDavid@users.noreply.github.com>
2025-04-13 21:01:17 +0700

    menambahkan file env dan konfigurasi dari connection serta menambahkan gitignore dan vendor

--- Repository/connection.php
+++ Repository/connection.php
@@ -1,7 +1,13 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$pass = '';
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
 $db   = 'projectMagang1';
 
 $conn = mysqli_connect($host, $user, $pass, $db);
